3c708601bdc61f9443407137ca8bbba400348c7a
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-03 13:24:05 +0200

    Update ProductsController.java

--- src/main/java/com/chumz/crud/controllers/ProductsController.java
+++ src/main/java/com/chumz/crud/controllers/ProductsController.java
@@ -5,6 +5,7 @@ import com.chumz.crud.services.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class ProductsController {
     @Autowired
     private ProductRepository productRepository;
 
+    @GetMapping({"", "/"})
     public String showProductList(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
